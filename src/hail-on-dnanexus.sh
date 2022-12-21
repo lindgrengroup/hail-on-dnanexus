8687321fe5ed5a9052c49b840c37a197f4a91a12
Barney Hill <barneyh@NDPH9082.local>
2022-12-21 17:08:00 +0000

    Now working with DNAnexus storage

--- src/hail-on-dnanexus.sh
+++ src/hail-on-dnanexus.sh
@@ -1,11 +1,22 @@
 main() {
 	export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
-	apt-get install libopenblas-dev liblapack-dev liblz4-dev -y
-	git clone https://github.com/hail-is/hail.git
-	cd hail/hail
-	sed -i 's/$(PIP) install/$(PIP) install --ignore-installed/g' Makefile
-	sed -i 's/check-pip-lockfiles/check-pip-lockfile/g' Makefile
-	make install-on-cluster HAIL_COMPILE_NATIVES=1 SCALA_VERSION=2.12.15 SPARK_VERSION=3.2.0
+	
+	start=`date +%s`
+		
+	#apt-get install libopenblas-dev liblapack-dev liblz4-dev -y
+	#git clone https://github.com/hail-is/hail.git
+	#cd hail/hail
+	#sed -i 's/$(PIP) install/$(PIP) install --ignore-installed/g' Makefile
+	#sed -i 's/check-pip-lockfiles/check-pip-lockfile/g' Makefile
+	#make install-on-cluster HAIL_COMPILE_NATIVES=1 SCALA_VERSION=2.12.15 SPARK_VERSION=3.2.0
+	
+	sed '/^pyspark/d' /pinned-requirements.txt | grep -v -e '^[[:space:]]*#' -e '^$$' | tr '\n' '\0' | xargs -0 pip install --ignore-installed -U
+        pip uninstall -y hail
+        pip install --ignore-installed /hail-0.2.107-py3-none-any.whl --no-deps
+        hailctl config set query/backend spark
+
+	end=`date +%s`
+	echo $((end-start))
 
 	MOUNTED_HOME=/home/dnanexus
 	DX_CONFIG=$MOUNTED_HOME/environment
@@ -15,7 +26,7 @@ main() {
 	export PYTHONPATH=":$SPARK_HOME/python"
 	export PYTHONPATH="$PYTHONPATH:$(echo "$SPARK_HOME/python/lib/py4j-"*"-src.zip")"
 
-	pip install dxdata
+	#pip install dxdata
 
 	HAIL_HOME=/usr/local/lib/python3.8/dist-packages/hail/backend
 	PYSPARK_SUBMIT_STR="\
@@ -26,9 +37,9 @@ main() {
 		--conf spark.kryo.registrator=is.hail.kryo.HailKryoRegistrator \
 		--conf spark.executorEnv.PYTHONPATH=$PYTHONPATH"
         LOG_CONFIG_PATH="/cluster/dnax/config/log/log4j-DEBUG.properties"
-        PYSPARK_SUBMIT_STR="${PYSPARK_SUBMIT_STR} \
-              --conf spark.executor.extraJavaOptions=-Dlog4j.configuration=file:$LOG_CONFIG_PATH \
-              --driver-java-options -Dlog4j.configuration=file:$LOG_CONFIG_PATH"
+        #PYSPARK_SUBMIT_STR="${PYSPARK_SUBMIT_STR} \
+        #      --conf spark.executor.extraJavaOptions=-Dlog4j.configuration=file:$LOG_CONFIG_PATH \
+        #      --driver-java-options -Dlog4j.configuration=file:$LOG_CONFIG_PATH"
 
 	PYSPARK_SUBMIT_STR="${PYSPARK_SUBMIT_STR} pyspark-shell"
 	export PYSPARK_SUBMIT_ARGS=${PYSPARK_SUBMIT_STR}
